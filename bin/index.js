02ad7e59454ce5f098f347a6f8a2af8d72e6435e
Jim Phieffer <phiefferj24@columbusacademy.org>
2022-05-08 18:46:30 -0400

    First commit

---
+++ bin/index.js
@@ -0,0 +1,105 @@
+#! /usr/bin/env node
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var express = require('express');
+var child_process = require("child_process");
+var os = require("os");
+var whitespaceRegex = /\s+(?=((\\[\\"]|[^\\"])*"(\\[\\"]|[^\\"])*")*(\\[\\"]|[^\\"])*$)/;
+var main = function () {
+    var _a, _b;
+    var args = process.argv.slice(process.argv.findIndex(function (elem) { return elem.includes("aidoku-serve"); }) + 1);
+    var outputDirectory = "./out/";
+    var portString = "3000";
+    var files = [];
+    for (var _i = 0, args_1 = args; _i < args_1.length; _i++) {
+        var arg = args_1[_i];
+        if (arg === outputDirectory || (arg + "/") === outputDirectory || arg === portString) {
+            continue;
+        }
+        else if (arg === '-o' || arg === '--output') {
+            outputDirectory = args[args.indexOf(arg) + 1];
+            if (!outputDirectory.endsWith('/')) {
+                outputDirectory += '/';
+            }
+            continue;
+        }
+        else if (arg === '-p' || arg === '--port') {
+            portString = args[args.indexOf(arg) + 1];
+            continue;
+        }
+        else {
+            files.push(arg);
+        }
+    }
+    if (files.length === 0) {
+        console.log("You need to specify at least one file.");
+        return;
+    }
+    console.log("Compiling sources...");
+    var tempDirectory = outputDirectory + "temp/";
+    if (fs.existsSync(tempDirectory)) {
+        fs.rmdirSync(tempDirectory, { recursive: true });
+    }
+    fs.mkdirSync(tempDirectory, { recursive: true });
+    var sourcesDirectory = outputDirectory + "sources/";
+    if (fs.existsSync(sourcesDirectory)) {
+        fs.rmdirSync(sourcesDirectory, { recursive: true });
+    }
+    fs.mkdirSync(sourcesDirectory, { recursive: true });
+    var iconsDirectory = outputDirectory + "icons/";
+    if (fs.existsSync(iconsDirectory)) {
+        fs.rmdirSync(iconsDirectory, { recursive: true });
+    }
+    fs.mkdirSync(iconsDirectory, { recursive: true });
+    var sources = [];
+    for (var _c = 0, files_1 = files; _c < files_1.length; _c++) {
+        var file = files_1[_c];
+        var fileName = file.split('/').pop();
+        if (!fileName) {
+            continue;
+        }
+        var fileFolder = fileName.substring(0, fileName.length - 4);
+        child_process.execSync("unzip ".concat(file, " -d ").concat(tempDirectory).concat(fileFolder));
+        var filePayloadFolder = tempDirectory + fileFolder + "/Payload/";
+        var jsonString = fs.readFileSync(filePayloadFolder + "source.json", 'utf8');
+        var json = JSON.parse(jsonString);
+        var source = json.info;
+        if (!source.hasOwnProperty("nsfw")) {
+            source.nsfw = 1;
+        }
+        var sourceFileName = "".concat(source.id, "-v").concat((_a = source.version) !== null && _a !== void 0 ? _a : 1, ".aix");
+        var imageFileName = "".concat(source.id, "-v").concat((_b = source.version) !== null && _b !== void 0 ? _b : 1, ".png");
+        source.file = sourceFileName;
+        source.icon = imageFileName;
+        fs.copyFileSync(file, sourcesDirectory + sourceFileName);
+        fs.copyFileSync(filePayloadFolder + "Icon.png", outputDirectory + "icons/" + imageFileName);
+        sources.push(source);
+        fs.rmdirSync(tempDirectory + fileFolder, { recursive: true });
+    }
+    fs.writeFileSync(outputDirectory + "index.json", JSON.stringify(sources));
+    fs.writeFileSync(outputDirectory + "index.min.json", JSON.stringify(sources).replace(whitespaceRegex, ""));
+    fs.rmdirSync(tempDirectory, { recursive: true });
+    console.log("Sources compiled.");
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var interfaceName in interfaces) {
+        var networkInterface = interfaces[interfaceName];
+        if (!networkInterface) {
+            continue;
+        }
+        for (var _d = 0, networkInterface_1 = networkInterface; _d < networkInterface_1.length; _d++) {
+            var address = networkInterface_1[_d];
+            if (address.family === 'IPv4') {
+                addresses.push(address.address);
+            }
+        }
+    }
+    var port = parseInt(portString);
+    var app = express();
+    app.use(express.static(outputDirectory));
+    app.listen(port, function () {
+        console.log("Broadcasting source list on: \n".concat(addresses.map(function (val) { return "http://".concat(val, ":").concat(port, "/"); }).join("\n")));
+    });
+};
+main();
---
+++ index.ts
@@ -0,0 +1,106 @@
+#! /usr/bin/env node
+import * as fs from 'fs';
+const express = require('express');
+import * as child_process from 'child_process';
+import * as os from 'os';
+
+const whitespaceRegex = /\s+(?=((\\[\\"]|[^\\"])*"(\\[\\"]|[^\\"])*")*(\\[\\"]|[^\\"])*$)/;
+
+const main = () => {
+	let args = process.argv.slice(process.argv.findIndex(elem => elem.includes("aidoku-serve")) + 1)
+	let outputDirectory = "./out/"
+	let portString = "3000";
+	let files: string[] = [];
+	for (let arg of args) {
+		if (arg === outputDirectory || (arg + "/") === outputDirectory || arg === portString) {
+			continue;
+		}
+		else if (arg === '-o' || arg === '--output') {
+			outputDirectory = args[args.indexOf(arg) + 1];
+			if (!outputDirectory.endsWith('/')) {
+				outputDirectory += '/';
+			}
+			continue;
+		}
+		else if (arg === '-p' || arg === '--port') {
+			portString = args[args.indexOf(arg) + 1];
+			continue;
+		}
+		else {
+			files.push(arg);
+		}
+	}
+	if (files.length === 0) {
+		console.log("You need to specify at least one file.");
+		return;
+	}
+
+	console.log("Compiling sources...")
+
+	let tempDirectory = outputDirectory + "temp/";
+	if (fs.existsSync(tempDirectory)) {
+		fs.rmdirSync(tempDirectory, { recursive: true });
+	}
+	fs.mkdirSync(tempDirectory, { recursive: true });
+	let sourcesDirectory = outputDirectory + "sources/";
+	if (fs.existsSync(sourcesDirectory)) {
+		fs.rmdirSync(sourcesDirectory, { recursive: true });
+	}
+	fs.mkdirSync(sourcesDirectory, { recursive: true });
+	let iconsDirectory = outputDirectory + "icons/";
+	if (fs.existsSync(iconsDirectory)) {
+		fs.rmdirSync(iconsDirectory, { recursive: true });
+	}
+	fs.mkdirSync(iconsDirectory, { recursive: true });
+	let sources: object[] = [];
+
+	for (let file of files) {
+		let fileName = file.split('/').pop();
+		if (!fileName) {
+			continue;
+		}
+		let fileFolder = fileName!.substring(0, fileName.length - 4);
+		child_process.execSync(`unzip ${file} -d ${tempDirectory}${fileFolder}`);
+		let filePayloadFolder = tempDirectory + fileFolder + "/Payload/";
+		let jsonString = fs.readFileSync(filePayloadFolder + "source.json", 'utf8');
+		let json = JSON.parse(jsonString);
+		let source = json.info;
+		if (!source.hasOwnProperty("nsfw")) {
+			source.nsfw = 1;
+		}
+		let sourceFileName = `${source.id}-v${source.version ?? 1}.aix`;
+		let imageFileName = `${source.id}-v${source.version ?? 1}.png`;
+		source.file = sourceFileName;
+		source.icon = imageFileName;
+		fs.copyFileSync(file, sourcesDirectory + sourceFileName);
+		fs.copyFileSync(filePayloadFolder + "Icon.png", outputDirectory + "icons/" + imageFileName);
+		sources.push(source);
+		fs.rmdirSync(tempDirectory + fileFolder, { recursive: true });
+	}
+	fs.writeFileSync(outputDirectory + "index.json", JSON.stringify(sources));
+	fs.writeFileSync(outputDirectory + "index.min.json", JSON.stringify(sources).replace(whitespaceRegex, ""));
+	fs.rmdirSync(tempDirectory, { recursive: true });
+
+	console.log("Sources compiled.");
+
+	let interfaces = os.networkInterfaces();
+	let addresses: string[] = [];
+	for (let interfaceName in interfaces) {
+		let networkInterface = interfaces[interfaceName];
+		if (!networkInterface) {
+			continue;
+		}
+		for (let address of networkInterface) {
+			if (address.family === 'IPv4') {
+				addresses.push(address.address);
+			}
+		}
+	}
+	let port = parseInt(portString);
+	let app = express();
+	app.use(express.static(outputDirectory));
+	app.listen(port, () => {
+		console.log(`Broadcasting source list on: \n${addresses.map((val) => `http://${val}:${port}/`).join("\n")}`);
+	});
+}
+main();
